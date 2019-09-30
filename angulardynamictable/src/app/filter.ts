c2a5f293a02aad5d7790fed7736db9b52143e7b2
Lakshu06 <lakshitasaini06@gmail.com>
2019-09-30 18:37:34 +0530

    Reusable Table

--- angulardynamictable/src/app/filter.ts
+++ angulardynamictable/src/app/filter.ts
@@ -8,8 +8,8 @@ export class FilterPipe implements PipeTransform {
     transform(items: any[], field: string, value: any): any[] {
 
         if (!items) {
-            return []; 
-               }
+            return [];
+        }
 
         if (!field || !value) {
             return items;
